c16534567b352341b85903221643e505142c2d14
Atharva Shembade <atharvashembade@Atharvas-MBP.lan>
2024-09-15 17:07:15 -0700

    Complete

---
+++ compare.py
@@ -0,0 +1,80 @@
+import json
+import csv
+import pandas as pd
+
+file1 = 'Google_Result1.json'
+file2 = 'hw1.json'
+file3 = '100QueriesSet1.txt'
+
+
+def compare_files(urls1, urls2):
+        sum_d_sq = 0
+        overlap = 0
+
+            
+        for idx2, res2 in enumerate(urls2, start=1):
+            if res2 in urls1:
+                overlap += 1
+                idx1 = urls1.index(res2) + 1
+                d = idx1 - idx2
+                d_sq = d ** 2
+                sum_d_sq += d_sq
+                
+        return sum_d_sq, overlap
+    
+    
+def get_spearman(sum_d_sq, overlap:int):
+    if overlap == 1 and sum_d_sq == 0:
+        rho = 1
+    elif overlap == 1:
+        rho = 0
+    elif overlap == 0:
+        rho = 0
+    else:
+        rho = 1 - (6 * sum_d_sq)/(overlap*(overlap  ** 2 - 1))
+        
+    return rho
+
+
+# def save_csv(query, overlap, rho, filename='hw1.csv'):
+#     with open(filename, 'a', newline='') as csvfile:
+#         writer = csv.writer(csvfile)
+#         percent = overlap * 10
+#         writer.writerow([query, overlap, percent, rho])
+
+if __name__ == "__main__":
+    df = pd.DataFrame(columns=['Queries', 'Number of Overlapping Results', 'Percent Overlap', 'Spearman Coefficient'])
+    
+    with open(file3, 'r') as file:
+        for line in file:
+            query = line.strip()
+    # query = 'How do you replace coolant thermostat'
+    
+            with open(file1, 'r') as f1, open(file2, 'r') as f2:
+                    data1 = json.load(f1)
+                    data2 = json.load(f2)
+                    
+                    urls1 = data1[query]
+                    urls2 = data2[query]
+                    
+                    sum_d_sq, overlap = compare_files(urls1, urls2)
+                    rho = get_spearman(sum_d_sq, overlap)
+                    percent = round((overlap / len(urls2)) * 100)
+                    
+                    new_row = pd.DataFrame({
+                        'Queries': [query],
+                        'Number of Overlapping Results': [overlap],
+                        'Percent Overlap': [percent],
+                        'Spearman Coefficient': [rho]
+                    })
+                    
+                    df = pd.concat([df, new_row], ignore_index=True)
+                    
+    df.to_csv('hw1.csv', index=False)
+                
+                
+    
+    
+    
+    
+    
---
+++ main2.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+import time
+import requests
+from random import randint
+from html.parser import HTMLParser
+from tqdm import tqdm
+
+USER_AGENT = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'}
+
+class SearchEngine:
+    @staticmethod
+    def search(query, sleep=True):
+        if sleep: 
+            time.sleep(randint(2, 20))
+        temp_url = '+'.join(query.split())
+
+        url = 'https://www.bing.com/search?q=' + temp_url
+        soup = BeautifulSoup(requests.get(url, headers=USER_AGENT).text,"html.parser")
+        new_results = SearchEngine.scrape_search_result(soup)
+        return new_results
+    
+    @staticmethod
+    def scrape_search_result(soup):
+        raw_results = soup.find_all('li', {'class': 'b_algo'})
+        results = []
+        seen_links = []
+
+        for result in raw_results:
+            link_tag = result.find('a')
+            if link_tag and 'href' in link_tag.attrs:
+                link = link_tag['href']
+                
+                if link not in seen_links:
+                    seen_links.append(link)
+                    results.append(link)
+                    
+                if len(results) == 10:
+                    break 
+                
+        return results
+    
+    @staticmethod
+    def compare_files(file1, file2):
+        with open(file1, 'r') as f1, open(file2, 'r') as f2:
+            data1 = json.load(f1)
+            data2 = json.load(f2)
+            
+        if set(data1.keys()) != set(data2.keys()):
+            print("Queries don't match")
+            return
+
+if __name__=="__main__":
+    import json
+    results_dict = {}  # Dictionary to store queries and their results
+    with open('100QueriesSet1.txt', 'r') as file:
+        for line in tqdm(file):
+            query = line.strip()
+            if query:  # Ensure the line is not empty
+                search_results = SearchEngine.search(query)
+                results_dict[query] = search_results  # Store results in the dictionary
+
+    # Write the results dictionary to a JSON file
+    with open('hw1.json', 'w') as json_file:
+        json.dump(results_dict, json_file, indent=4)
+        
+    
